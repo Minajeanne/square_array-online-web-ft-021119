fa541fa7b8fcc6c6fc0540b0952a56f64cfccd00
Alicia Santiago <albrown07@mac.com>
2019-02-11 19:45:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_square_array=[]
   array.each do|number|
-  new_square_array << number.sqrt(number)
+  new_square_array << number.sqrt
   end
 return new_square_array
 end
